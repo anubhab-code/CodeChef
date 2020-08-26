9563b7b55d267b9db5bea94dd27b82b225b0753f
Anubhab Swain <anubhabswain69@gmail.com>
2020-08-26 10:19:22 +0530

    Create CHEFWARS.py

---
+++ Practice/Beginner/CHEFWARS.py
@@ -0,0 +1,15 @@
+tc=int(input())
+for i in range(tc):
+    h,p=map(int,input().split())
+    while h>0:
+        h = h - p
+        p = p // 2
+        if p>h:
+            print(1)
+            break
+        elif p==0 and h!=0:
+            print(0)
+            break
+        elif p==0 and h==0:
+            print(1)
+            break

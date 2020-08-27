7704248e857e571efaec984b38931b4bb3675dcc
Anubhab Swain <anubhabswain69@gmail.com>
2020-08-27 17:35:41 +0530

    Create ADASTAIR.py

---
+++ Practice/Beginner/ADASTAIR.py
@@ -0,0 +1,16 @@
+tc=int(input())
+for _ in range(tc):
+    n,k=map(int,input().split())
+    a=list(map(int,input().split()))
+    curr=0
+    res=0
+    for i in range(n):
+        if a[i]-curr<=k:
+            curr=a[i]
+        else:
+            if (a[i]-curr)%k==0:
+                res+=((a[i]-curr)//k)-1
+            else:
+                res+=(a[i]-curr)//k
+            curr=a[i]
+    print(res)
